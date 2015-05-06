14573518cf3a8eb34889b3e1ebebfef8fb6bfceb
no-tech <wjakesta@gmail.com>
2015-05-06 13:17:20 -0700

    added test_11.rb

---
+++ spec/test_12.rb
@@ -0,0 +1,15 @@
+require_relative 'spec_helper'
+
+# describe Barracks do
+
+#   before :each do
+#     @barracks = Barracks.new
+#     @footman = Footman.new
+#   end
+
+#   it "health should equals to 495 when footman attacks" do
+#     @footman.attack!(@barracks)
+#     expect(@barracks.health_points).to eq(495)
+#   end
+
+end
